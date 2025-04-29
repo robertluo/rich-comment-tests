19a1e0745a3ca75d26664308da88465877ae8425
Luo Tian <lt@basecity.com>
2025-04-29 15:28:21 +0800

    Clojars (#6)
    * bump dep versions
    
    * fix symbol read problem, bump version
    
    * clojure build and github workflow
    
    * get ready for clojars deploy

--- src/com/mjdowney/rich_comment_tests.clj
+++ src/com/mjdowney/rich_comment_tests.clj
@@ -231,21 +231,6 @@
        (do ~@body)
        (string/trim (.toString sw#)))))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defmacro with-printlns
-  "Rebind `println` during the execution of body to print as normal, PLUS
-  save each line of output, and return a vector of printed lines."
-  [& body]
-  `(let [println# clojure.core/println
-         printed# (atom [])]
-     (with-redefs [println (fn [& args#]
-                             (apply println# args#)
-                             (binding [*print-readably* nil]
-                               (swap! printed# conj (apply pr-str args#)))
-                             nil)]
-       ~@body
-       @printed#)))
-
 ^:rct/test
 (comment
   ;; Tests for things which should fail
@@ -326,6 +311,9 @@
   ;;current ns alias can be resolved on both sides
   :com.mjdowney.rich-comment-tests/ok ;=> ::ok
   ::z/opts ;=> :rewrite-clj.zip/opts
+  
+  '{a/a 2} ;=> 
+  '{a/a 2}
   )
 
 (comment ;; For example...
